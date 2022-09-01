6caaf4a20dd755e9b8d1addc183d2f872f2b9afd
yunhozz <qkrdbsgh1121@naver.com>
2022-09-01 18:15:09 +0900

    Chore: Initial commit

---
+++ src/test/java/colfume/ColfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package colfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
