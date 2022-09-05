19e5a20c9c7db4293d0afa1b573d04be8552ea79
yunhozz <qkrdbsgh1121@naver.com>
2022-09-05 16:20:50 +0900

    Feat: 기존 비밀번호 일치 예외 추가

---
+++ src/main/java/colfume/exception/PasswordSameException.java
@@ -0,0 +1,14 @@
+package colfume.exception;
+
+import colfume.enums.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PasswordSameException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public PasswordSameException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+}
