2e65f21df0cb943881e8ae2e79d60acb5d88f170
yunhozz <qkrdbsgh1121@naver.com>
2022-09-19 18:28:09 +0900

    Fix: key 삭제

--- src/main/java/colfume/enums/ColorType.java
+++ src/main/java/colfume/enums/ColorType.java
@@ -7,11 +7,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ColorType {
 
-    RED("COLOR_RED", "빨간색"),
-    ORANGE("COLOR_ORANGE", "주황색"),
-    YELLOW("COLOR_YELLOW", "노란색"),
-    GREEN("COLOR_GREEN", "초록색");
+    RED("빨간색"),
+    ORANGE("주황색"),
+    YELLOW("노란색"),
+    GREEN("초록색");
 
-    private final String key;
     private final String value;
 }
