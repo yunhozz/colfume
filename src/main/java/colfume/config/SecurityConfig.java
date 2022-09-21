223d55374a130867264424cb67ff98da6af52cd2
yunhozz <qkrdbsgh1121@naver.com>
2022-09-21 16:49:20 +0900

    Fix: 카카오 클라이언트 서비스 삭제

--- src/main/java/colfume/config/SecurityConfig.java
+++ src/main/java/colfume/config/SecurityConfig.java
@@ -12,11 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -33,11 +28,6 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(kakaoClientRegistration());
     }
 
     @Bean
@@ -73,22 +63,5 @@ public class SecurityConfig {
                 .userService(oAuth2UserService);
 
         return httpSecurity.build();
-    }
-
-    // 카카오 클라이언트 등록
-    private ClientRegistration kakaoClientRegistration() {
-        return ClientRegistration.withRegistrationId("kakao")
-                .clientId("")
-                .clientSecret("")
-                .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .scope("profile_nickname", "profile_image", "account_email")
-                .clientName("kakao")
-                .authorizationUri("https://kauth.kakao.com/oauth/authorize")
-                .tokenUri("https://kauth.kakao.com/oauth/token")
-                .userInfoUri("https://kapi.kakao.com/v2/user/me")
-                .userNameAttributeName("id")
-                .build();
     }
 }
