a1870e9e6ef2b2534de22b0c72ea4eb716120b40
yunhozz <qkrdbsgh1121@naver.com>
2022-09-25 17:40:38 +0900

    Feat: jwt 재발급 토큰에 관한 db 추가

---
+++ src/main/java/colfume/oauth/jwt/UserRefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package colfume.oauth.jwt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRefreshTokenRepository extends JpaRepository<UserRefreshToken, Long> {
+
+    Optional<UserRefreshToken> findByUserId(Long userId);
+    Optional<UserRefreshToken> findByUserIdAndRefreshToken(Long userId, String refreshToken);
+}
