0266a68d25bba9d02633cbe7e264e4f4ea3c03a8
yunhozz <qkrdbsgh1121@naver.com>
2022-09-27 16:47:09 +0900

    Fix: http 상태 코드 추가

--- src/main/java/colfume/api/dto/Response.java
+++ src/main/java/colfume/api/dto/Response.java
@@ -3,6 +3,7 @@ package colfume.api.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
@@ -11,20 +12,21 @@ public class Response {
     private boolean success;
     private int code;
     private Result result;
+    private HttpStatus status;
 
-    public static Response success() {
-        return new Response(true, 0, null);
+    public static Response success(HttpStatus status) {
+        return new Response(true, 0, null, status);
     }
 
-    public static <T> Response success(T data) {
-        return new Response(true, 0, new Success<>(data));
+    public static <T> Response success(T data, HttpStatus status) {
+        return new Response(true, 0, new Success<>(data), status);
     }
 
-    public static Response failure(int code, String message) {
-        return new Response(false, code, new Failure<>(message));
+    public static Response failure(int code, String message, HttpStatus status) {
+        return new Response(false, code, new Failure<>(message), status);
     }
 
-    public static <T> Response failure(int code, T data) {
-        return new Response(false, code, new Failure<>(data));
+    public static <T> Response failure(int code, T data, HttpStatus status) {
+        return new Response(false, code, new Failure<>(data), status);
     }
 }
