5c9038fdc140e476bee4cbdf13852eb40e2bdab5
yunhozz <qkrdbsgh1121@naver.com>
2022-09-29 18:09:14 +0900

    Feat: 만든이 데이터 추가

---
+++ src/main/java/colfume/domain/BaseTimeUser.java
@@ -0,0 +1,23 @@
+package colfume.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public class BaseTimeUser extends BaseTime {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModifiedBy;
+}
