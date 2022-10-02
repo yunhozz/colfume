630a979e44bc8f9796a782d7432ab6513c57a78b
yunhozz <qkrdbsgh1121@naver.com>
2022-10-02 19:30:10 +0900

    Feat: 평가 점수, 삭제 여부 컬럼 추가

--- src/main/java/colfume/domain/evaluation/model/entity/Evaluation.java
+++ src/main/java/colfume/domain/evaluation/model/entity/Evaluation.java
@@ -3,6 +3,8 @@ package colfume.domain.evaluation.model.entity;
 import colfume.domain.BaseTime;
 import colfume.domain.member.model.entity.Member;
 import colfume.domain.perfume.model.entity.Perfume;
+import colfume.enums.ErrorCode;
+import colfume.exception.AlreadyDeletedException;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,14 +31,29 @@ public class Evaluation extends BaseTime {
 
     private String content;
 
+    @Column(columnDefinition = "tinyint")
+    private int score;
+
+    private boolean isDeleted;
+
     @Builder
-    private Evaluation(Member writer, Perfume perfume, String content) {
+    private Evaluation(Member writer, Perfume perfume, String content, int score) {
         this.writer = writer;
         this.perfume = perfume;
         this.content = content;
+        this.score = score;
     }
 
-    public void updateContent(String content) {
+    public void update(String content, int score) {
         this.content = content;
+        this.score = score;
+    }
+
+    public void delete() {
+        if (!isDeleted) {
+            isDeleted = true;
+        } else {
+            throw new AlreadyDeletedException(ErrorCode.ALREADY_DELETED);
+        }
     }
 }
