915eccf1524195f1c5402568c8f401060d6ff1af
yunhozz <qkrdbsgh1121@naver.com>
2022-10-31 19:01:28 +0900

    remove: 필요없는 파일 삭제

--- src/main/java/colfume/domain/member/model/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package colfume.domain.member.model.repository;
-
-import colfume.domain.member.model.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
