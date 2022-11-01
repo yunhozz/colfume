a88177c9c317e270a087beccdfcdfff85e093c2e
yunhozz <qkrdbsgh1121@naver.com>
2022-11-01 19:36:29 +0900

    fix: oauth 테스트를 위한 수정

--- src/main/java/colfume/common/HomeController.java
+++ src/main/java/colfume/common/HomeController.java
@@ -1,26 +1,29 @@
 package colfume.common;
 
-import colfume.common.util.CookieUtils;
+import colfume.oauth.UserPrincipal;
+import colfume.oauth.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+    private final JwtProvider jwtProvider;
+
     @GetMapping("/")
-    public String home(HttpServletRequest request, HttpServletResponse response, Model model) {
-        Optional<Cookie> accessToken = CookieUtils.getCookie(request, "access-token");
-        if (accessToken.isEmpty()) {
-            return "home";
+    public String home(@RequestParam(required = false) String token, Model model) {
+        if (token != null) {
+            Authentication authentication = jwtProvider.getAuthentication(token);
+            UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+            model.addAttribute("principal", userPrincipal);
+
+            return "login-home";
         }
-        return "login-home";
+        return "home";
     }
 }
