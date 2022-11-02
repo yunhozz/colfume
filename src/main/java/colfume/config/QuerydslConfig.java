718b56528a7f28664c25f8f5cb8fec466458414e
yunhozz <qkrdbsgh1121@naver.com>
2022-11-02 13:13:25 +0900

    fix: 'EntityManager' 주입 방식 변경

--- src/main/java/colfume/config/QuerydslConfig.java
+++ src/main/java/colfume/config/QuerydslConfig.java
@@ -1,17 +1,17 @@
 package colfume.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Configuration
-@RequiredArgsConstructor
 public class QuerydslConfig {
 
-    private final EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
