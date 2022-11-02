c87930c3e078d99daea1f8fcbf43ed05f097cfd0
yunhozz <qkrdbsgh1121@naver.com>
2022-11-02 13:06:13 +0900

    fix: validation 삭제

--- src/main/java/colfume/api/dto/perfume/SearchDto.java
+++ src/main/java/colfume/api/dto/perfume/SearchDto.java
@@ -5,17 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SearchDto {
 
-    @NotBlank(message = "키워드를 입력해주세요.")
     private String keyword;
-
-    @NotNull
     private SearchCondition condition; // LATEST, ACCURACY
 }
