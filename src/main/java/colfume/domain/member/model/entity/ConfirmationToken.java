9ad7bd92b4d4e5b406208107a0bfeac15934b866
yunhozz <qkrdbsgh1121@naver.com>
2022-11-07 12:19:18 +0900

    fix: user id 값만 저장하도록 설정

--- src/main/java/colfume/domain/member/model/entity/ConfirmationToken.java
+++ src/main/java/colfume/domain/member/model/entity/ConfirmationToken.java
@@ -5,10 +5,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
@@ -20,6 +24,11 @@ public class ConfirmationToken extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    private Member user;
+
+    @Column(name = "user_id", length = 64, unique = true)
     private Long userId;
 
     private LocalDateTime expirationDate; // 만료 시간
--- src/main/java/colfume/oauth/model/UserRefreshToken.java
+++ src/main/java/colfume/oauth/model/UserRefreshToken.java
@@ -1,11 +1,19 @@
 package colfume.oauth.model;
 
 import colfume.domain.BaseTime;
+import colfume.domain.member.model.entity.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -16,7 +24,11 @@ public class UserRefreshToken extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 64, unique = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(insertable = false, updatable = false)
+    private Member user;
+
+    @Column(name = "user_id", length = 64, unique = true)
     private Long userId;
 
     private String refreshToken;
