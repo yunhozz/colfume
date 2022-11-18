0080d6acac7f0eeed45d47ccee65dcbfe77d02a3
yunhozz <qkrdbsgh1121@naver.com>
2022-11-18 18:39:48 +0900

    feat: 알림 조회시 연관 엔티티 fetch join 하는 쿼리 작성

--- src/main/java/colfume/domain/notification/model/repository/NotificationRepository.java
+++ src/main/java/colfume/domain/notification/model/repository/NotificationRepository.java
@@ -2,6 +2,13 @@ package colfume.domain.notification.model.repository;
 
 import colfume.domain.notification.model.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long>, NotificationRepositoryCustom {
+
+    @Query("select n from Notification n join fetch n.sender s join fetch n.receiver r where n.id = :id")
+    Optional<Notification> findWithSenderAndReceiverById(@Param("id") Long id);
 }
