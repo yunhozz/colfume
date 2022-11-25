312a13e6a8d9cd350e2c5575f912adadcad2270a
yunhozz <qkrdbsgh1121@naver.com>
2022-11-25 19:04:13 +0900

    feat: Security 관련 예외를 리턴할 수 있도록 로직 추가

--- src/main/java/colfume/common/GlobalExceptionHandler.java
+++ src/main/java/colfume/common/GlobalExceptionHandler.java
@@ -26,6 +26,8 @@ import colfume.domain.notification.service.exception.NotificationSendFailExcepti
 import colfume.domain.perfume.service.PerfumeNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -40,20 +42,33 @@ import static colfume.common.dto.ErrorResponseDto.NotValidResponseDto;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // TODO: 2022-11-25 에러 코드 수정
+
     @ExceptionHandler(RuntimeException.class)
     public Response handleRuntimeException(RuntimeException e) {
-        log.error("handleRuntimeException", e);
-        ErrorResponseDto error = new ErrorResponseDto(ErrorCode.INTER_SERVER_ERROR);
+        // Security 와 관련된 Exception 발생 이전에 RuntimeException 이 먼저 발생하므로 제외
+        if (e instanceof AccessDeniedException) {
+            log.error("handleAccessDeniedException", e);
+            ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.FORBIDDEN);
+            return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
+        }
 
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        if (e instanceof AuthenticationException) {
+            log.error("handleAuthenticationException", e);
+            ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.UNAUTHORIZED);
+            return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
+        }
+
+        log.error("handleRuntimeException", e);
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.INTER_SERVER_ERROR);
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public Response handleIllegalStateException(IllegalStateException e) {
         log.error("handleIllegalStateException", e);
-        ErrorResponseDto error = new ErrorResponseDto(ErrorCode.NOT_VALID);
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.NOT_VALID);
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -74,175 +89,154 @@ public class GlobalExceptionHandler {
             });
         }};
 
-        ErrorResponseDto error = new ErrorResponseDto(ErrorCode.NOT_VALID, notValidResponseDtoList);
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(ErrorCode.NOT_VALID, notValidResponseDtoList);
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(CrudNotAuthenticationException.class)
     public Response handleCrudNotAuthenticationException(CrudNotAuthenticationException e) {
         log.error("handleCrudNotAuthenticationException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(MemberNotFoundException.class)
     public Response handleMemberNotFoundException(MemberNotFoundException e) {
         log.error("handleMemberNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EmailNotFoundException.class)
     public Response handleEmailNotFoundException(EmailNotFoundException e) {
         log.error("handleEmailNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(ConfirmationTokenNotFoundException.class)
     public Response handleConfirmationTokenNotFoundException(ConfirmationTokenNotFoundException e) {
         log.error("handleConfirmationTokenNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EmailNotVerifiedException.class)
     public Response handleEmailNotVerifiedException(EmailNotVerifiedException e) {
         log.error("handleEmailNotVerifiedException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EmailDuplicateException.class)
     public Response handleEmailDuplicateException(EmailDuplicateException e) {
         log.error("handleEmailDuplicateException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(PasswordMismatchException.class)
     public Response handlePasswordMismatchException(PasswordMismatchException e) {
         log.error("handlePasswordMismatchException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(PasswordSameException.class)
     public Response handlePasswordSameException(PasswordSameException e) {
         log.error("handlePasswordSameException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(RefreshTokenNotFoundException.class)
     public Response handleRefreshTokenNotFoundException(RefreshTokenNotFoundException e) {
         log.error("handleRefreshTokenNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(RefreshTokenNotCorrespondException.class)
     public Response handleRefreshTokenNotCorrespondException(RefreshTokenNotCorrespondException e) {
         log.error("handleRefreshTokenNotCorrespondException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(PerfumeNotFoundException.class)
     public Response handlePerfumeNotFoundException(PerfumeNotFoundException e) {
         log.error("handlePerfumeNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(NotificationNotFoundException.class)
     public Response handleNotificationNotFoundException(NotificationNotFoundException e) {
         log.error("handleNotificationNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(NotificationSendFailException.class)
     public Response handleNotificationSendFailException(NotificationSendFailException e) {
         log.error("handleNotificationSendFailException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(ChatroomNotFoundException.class)
     public Response handleChatroomNotFoundException(ChatroomNotFoundException e) {
         log.error("handleChatroomNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(ChatroomPermissionException.class)
     public Response handleChatroomPermissionException(ChatroomPermissionException e) {
         log.error("handleChatroomPermissionException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(BookmarkNotFoundException.class)
     public Response handleBookmarkNotFoundException(BookmarkNotFoundException e) {
         log.error("handleBookmarkNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(BookmarkAlreadyCreatedException.class)
     public Response handleBookmarkAlreadyCreatedException(BookmarkAlreadyCreatedException e) {
         log.error("handleBookmarkAlreadyCreatedException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EvaluationNotFoundException.class)
     public Response handleEvaluationNotFoundException(EvaluationNotFoundException e) {
         log.error("handleEvaluationNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(CommentNotFoundException.class)
     public Response handleCommentNotFoundException(CommentNotFoundException e) {
         log.error("handleCommentNotFoundException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EvaluationAlreadyDeletedException.class)
     public Response handleEvaluationAlreadyDeletedException(EvaluationAlreadyDeletedException e) {
         log.error("handleEvaluationAlreadyDeletedException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 
     @ExceptionHandler(EvaluationAlreadyExistException.class)
     public Response handleEvaluationAlreadyExistException(EvaluationAlreadyExistException e) {
         log.error("handleEvaluationAlreadyExistException", e);
-        ErrorResponseDto error = new ErrorResponseDto(e.getErrorCode());
-
-        return Response.failure(-1000, error, HttpStatus.valueOf(error.getStatus()));
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(e.getErrorCode());
+        return Response.failure(-1000, errorResponseDto, HttpStatus.valueOf(errorResponseDto.getStatus()));
     }
 }
