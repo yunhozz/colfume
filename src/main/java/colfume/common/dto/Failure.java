a9f492115dad78ea2bf3d59cd09be2be3cb79845
yunhozz <qkrdbsgh1121@naver.com>
2022-11-25 19:13:01 +0900

    feat: timestamp 추가

--- src/main/java/colfume/common/dto/Failure.java
+++ src/main/java/colfume/common/dto/Failure.java
@@ -3,18 +3,23 @@ package colfume.common.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Failure<T> implements Result {
 
+    private final LocalDateTime timestamp;
     private final T data;
     private String message;
 
     public Failure(T data) {
+        timestamp = LocalDateTime.now();
         this.data = data;
     }
 
     public Failure(T data, String message) {
+        timestamp = LocalDateTime.now();
         this.data = data;
         this.message = message;
     }
