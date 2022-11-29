56e29c3ab7ca37fd838a48c3fb9676aae7f62c6b
yunhozz <qkrdbsgh1121@naver.com>
2022-11-29 17:31:30 +0900

    remove: 필요없는 파일 삭제

--- src/main/java/colfume/domain/bookmark/service/BookmarkConverter.java
+++ src/main/java/colfume/domain/bookmark/service/BookmarkConverter.java
@@ -1,17 +1,16 @@
 package colfume.domain.bookmark.service;
 
 import colfume.common.EntityConverter;
-import colfume.domain.bookmark.dto.BookmarkRequestDto;
 import colfume.domain.bookmark.dto.BookmarkResponseDto;
 import colfume.domain.bookmark.model.Bookmark;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class BookmarkConverter implements EntityConverter<Bookmark, BookmarkRequestDto, BookmarkResponseDto> {
+public class BookmarkConverter implements EntityConverter<Bookmark, Void, BookmarkResponseDto> {
 
     @Override
-    public Bookmark convertToEntity(BookmarkRequestDto bookmarkRequestDto) {
+    public Bookmark convertToEntity(Void v) {
         return null;
     }
 
